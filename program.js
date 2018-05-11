0e47cf2406bc8bec8388b4206e9250f37df6b725
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-05-11 13:53:56 +0200

    Change line 20

--- program.js
+++ program.js
@@ -17,7 +17,7 @@ process.stdin.on('readable', function() {
             console.log(process.env);
             break;
             default:
-            process.stdout.write('Wrong instruction!\n');
+            process.stderr.write('Wrong instruction!');
         }
     }
 });
