03be0682fe09062dbd3e266f97411533189a8003
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-05-11 13:45:19 +0200

    Add program.js

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    // tutaj treść tego co ma się wykonać w momencie odczytania wejścia.
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch(instruction) {
+            case '/exit': 
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+            case '/node_version':
+            console.log(process.versions);
+            break;
+            case '/node_language':
+            console.log(process.env);
+            break;
+            default:
+            process.stdout.write('Wrong instruction!\n');
+        }
+    }
+});
